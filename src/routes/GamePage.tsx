d4c0d1024ee4e420bc2fe8227d1e3bd78527704f
dohung <dohuyhung12@gmail.com>
2022-01-27 14:21:06 +0700

    dynamic import img in gamepage

--- src/routes/GamePage.tsx
+++ src/routes/GamePage.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import Loading from './Loading'
 
 const GamePage = () => {
   const param = useParams()
-  console.log(param)
-  return <div>Hello form gamepage </div>
+
+  const imgSrc = require(`../img/level/${param.id}.jpeg`)
+
+  return (
+    <div>
+      Hello form gamepage {param.id}
+      <img src={imgSrc} alt='' />
+    </div>
+  )
 }
 
 export default GamePage
